4f8f825e1950c85005431768f634b340a2713625
scliff6262 <sclifford6262@gmail.com>
2018-08-15 13:26:39 -0400

    removed slash in Access-control-allow-origin header

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BooksReact
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'https://books-react-frontend.herokuapp.com/',
+    'Access-Control-Allow-Origin' => 'https://books-react-frontend.herokuapp.com',
     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
   }
   end
